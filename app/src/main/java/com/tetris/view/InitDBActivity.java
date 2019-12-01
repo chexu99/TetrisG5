3f4802fc68176c5d161e052f1f4aedbfcbf3a77c
rodrigo <rodri6diezalonso@gmail.com>
2019-12-01 17:39:39 +0100

    Photo finish

--- app/src/main/java/com/tetris/view/InitDBActivity.java
+++ app/src/main/java/com/tetris/view/InitDBActivity.java
@@ -1,10 +1,12 @@
 package com.tetris.view;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
@@ -12,23 +14,35 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.multidex.MultiDex;
 
 import com.tetris.R;
 import com.tetris.model.database.ConexionSQLiteHelper;
 import com.tetris.utils.UserSettings;
 
-public class InitDBActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class InitDBActivity extends AppCompatActivity   {
+
     ConexionSQLiteHelper conn;
     SQLiteDatabase db;
     Integer puntuacion;
     String username;
     EditText campo_nombre;
     Cursor cursor;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    //ImageView foto;
+    static final int REQUEST_IMAGE_CAPTURE = 20;
+    private final String carpeta_raiz="document/";
+    private final String ruta_imagen=carpeta_raiz+"primary:Pictures/.thumbnails/";
+    String absolutePath="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        MultiDex.install(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         conn = new ConexionSQLiteHelper(getApplicationContext(), "db_ranking", null, 1);
@@ -38,11 +52,6 @@ public class InitDBActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         usernameComparator();
-        photo();
-
-
-        /*Intent intent = new Intent(InitDBActivity.this, MenuActivity.class);
-        startActivity(intent);*/
     }
 
     private void usernameComparator() {
@@ -52,8 +61,10 @@ public class InitDBActivity extends AppCompatActivity {
         cursor = db.rawQuery("SELECT * FROM ranking WHERE nombre ='" + username + "'", null);
         if (cursor.moveToFirst()) { // si cursor no apunta a nada entra por el else
             login();
+            Intent intent = new Intent(InitDBActivity.this, MenuActivity.class);
+            startActivity(intent);
         } else {
-            register();
+            photo();
         }
     }
 
@@ -66,42 +77,81 @@ public class InitDBActivity extends AppCompatActivity {
         UserSettings.setScore(lastHighScore);
         cursor.close();
         db.close();
-
-        Toast.makeText(getApplicationContext(), "Hola de nuevo " + username, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Hola de nuevo "+username, Toast.LENGTH_LONG).show();
 
 
     }
 
-    private void register() {
+    private void register(String img) {
         Integer id;
         puntuacion = 0;
         cursor.close();
+        ContentValues values;
+
         Cursor cursor2 = db.rawQuery("SELECT * FROM ranking", null);
         if (cursor2.moveToLast()) {
             id = cursor2.getInt(0) + 1;
         } else {
             id = 1;
         }
-        Toast.makeText(getApplicationContext(), "registrado", Toast.LENGTH_LONG).show();
         db = conn.getWritableDatabase();
-        String insert = "INSERT INTO ranking(id,nombre,score) VALUES (" + id + ",'" + username + "'," + puntuacion + ")";
-        db.execSQL(insert);
+        try{
+            FileInputStream fs = new FileInputStream(img);
+            byte[] imgbyte = new byte[fs.available()];
+            fs.read(imgbyte);
+            values = new ContentValues();
+            values.put("id",id);
+            values.put("nombre",username);
+            values.put("score",puntuacion);
+            values.put("img",imgbyte);
+            db.insert("ranking",null,values);
+            fs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         cursor2.close();
         db.close();
     }
 
-    private void photo(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
-            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+    private void photo() {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePhotoIntent.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            try{
+                photoFile = createPhotoFile();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            if (photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(InitDBActivity.this,"com.tetris.provider",photoFile);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                if(takePhotoIntent.resolveActivity(getPackageManager())!=null) {
+                    startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
         }
     }
+
+    private File createPhotoFile() throws IOException {
+        String photoName = new SimpleDateFormat("ddMMyyyy HHmmss").format(new Date());
+        File storageFile = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photoFile = File.createTempFile(
+                photoName,
+                ".jpg",
+                storageFile
+        );
+        absolutePath=photoFile.getAbsolutePath();
+        return photoFile;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //foto.setImageBitmap(imageBitmap);
+            register(absolutePath);
+            Toast.makeText(getApplicationContext(), "registrado", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(InitDBActivity.this, MenuActivity.class);
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/tetris/view/RankingActivity.java
+++ app/src/main/java/com/tetris/view/RankingActivity.java
@@ -1,35 +1,39 @@
 package com.tetris.view;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.tetris.R;
 import com.tetris.model.Player;
 import com.tetris.model.database.ConexionSQLiteHelper;
+import com.tetris.utils.RecyclerViewAdaptador;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RankingActivity extends AppCompatActivity {
     ImageButton button;
     ListView LRanking;
     ArrayList<String> listaInfo;
-    ArrayList<Player> listaPlayers;
     ConexionSQLiteHelper conn;
+    private RecyclerView recyclerViewUser;
+    private RecyclerViewAdaptador userAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
-
         //boton de volver
         button = findViewById(R.id.reStar);
         button.setOnClickListener(new View.OnClickListener() {
@@ -38,20 +42,19 @@ public class RankingActivity extends AppCompatActivity {
                 openPreviousActivity();
             }
         });
-
-        LRanking = (ListView) findViewById(R.id.L_ranking);
+        recyclerViewUser=(RecyclerView)findViewById(R.id.recyclerUser);
+        recyclerViewUser.setLayoutManager(new LinearLayoutManager(this));
 
         conn = new ConexionSQLiteHelper(getApplicationContext(), "db_ranking", null, 1);
-        checkPlayerList();
 
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_white_text, R.id.list_content, listaInfo);
-        LRanking.setAdapter(adapter);
+        userAdapter = new RecyclerViewAdaptador(checkPlayerList());
+        recyclerViewUser.setAdapter(userAdapter);
     }
 
-    private void checkPlayerList() {
+    public List<Player> checkPlayerList() {
         SQLiteDatabase db = conn.getReadableDatabase();
         Player player;
-        listaPlayers = new ArrayList<>();
+        ArrayList<Player> listaPlayers = new ArrayList<>();
         Cursor cursor = db.rawQuery("SELECT * FROM ranking ORDER BY score DESC", null);
 
         while (cursor.moveToNext()) {
@@ -59,18 +62,13 @@ public class RankingActivity extends AppCompatActivity {
             player.setId(cursor.getInt(0));
             player.setNombre(cursor.getString(1));
             player.setScore(cursor.getInt(2));
+            player.setImg(cursor.getBlob(3));
 
             listaPlayers.add(player);
         }
-        getPlayerList();
+        return listaPlayers;
     }
 
-    private void getPlayerList() { //TODO: se necesita para algo?
-        listaInfo = new ArrayList<>();
-        for (int i = 0; i < listaPlayers.size(); i++) {
-            listaInfo.add(listaPlayers.get(i).getNombre() + " - " + listaPlayers.get(i).getScore());
-        }
-    }
 
     private void openPreviousActivity() {
         finish();
